279c54205dd99511923e30799e55633b3b65b01e
jrscott-goggles <justin@scottfamily.com>
2016-03-23 16:47:02 -0700

    added check for valid artist id and changed flow
    Added check to add_album() to ensure user entered artist id was valid
    Changed program flow so main_menu() runs in a while loop with an exit
    condition changable in exit_program() instead of each option calling it
    again

--- base.py
+++ base.py
@@ -8,6 +8,8 @@ SQL_INSERT_ALBUM = 'INSERT INTO albums(name, artist_id) VALUES(?,?);'
 SQL_SELECT_ARTISTS = 'SELECT id, name FROM artists'
 SQL_INSERT_ARTIST = 'INSERT INTO artists(name) VALUES(?)'
 
+stay = True
+
 def main_menu():
 	print('Welcome to the music database!')
 	print('\t1. Display all song information.')
@@ -33,68 +35,71 @@ def main_menu():
 def display_songs():
 	#display and add songs
 	print('the songs')
-	main_menu()
 
 def add_genre():
 	#prints genres
 	print('Genres in the database:')
 	for row in cursor.execute(SQL_SELECT_GENRES):
 		print('%d. %s' % (int(row[0]), row[1]))
-	new_genre = str(raw_input('Enter new genre name or leave blank to exit to menu:'))
+	new_genre = str(raw_input('Enter new genre name or stay blank to exit to menu:'))
 	#if something was entered, add it as a genre
 	if (len(new_genre) != 0): cursor.execute(SQL_INSERT_GENRE,(new_genre,))
-	main_menu()
-	
 
 def add_album():
 	#prints albums
 	print('Albums in the database:')
 	for row in cursor.execute(SQL_SELECT_ALBUMS):
 		print('%d. Album: %s. Artist: %s' % (int(row[0]), row[1], row[2]))
-	new_album = str(raw_input('Enter new album name or leave blank to exit to menu: '))
+	new_album = str(raw_input('Enter new album name or stay blank to exit to menu: '))
 	if (len(new_album) != 0):
+		artist_id_list = []
 		for row in cursor.execute(SQL_SELECT_ARTISTS):
 			print('%d. %s' %(int(row[0]), row[1]))
-		artist = str(raw_input('Enter the number of the artist for this album or leave blank to exit to menu: '))
-		#add error checking for invalid artist
-		if(len(artist) != 0): cursor.execute(SQL_INSERT_ALBUM,(new_album, artist,));
-	main_menu()
-	
+			artist_id_list.append(int(row[0]))
+		
+		artist_id = raw_input('Enter the number of the artist for this album or stay blank to exit to menu: ')
+		try:
+			artist_id = int(artist_id)
+			if(artist_id in artist_id_list): cursor.execute(SQL_INSERT_ALBUM,(new_album, artist_id,))
+			elif(len(artist_id) != 0): invalid_input()
+		except ValueError:
+			if(len(str(artist_id)) != 0): invalid_input()
+
 def add_artist():
 	for row in cursor.execute(SQL_SELECT_ARTISTS):
 		print('%d. %s' %(int(row[0]), row[1]))
-	new_artist = str(raw_input('Enter new artist name or leave blank to exit to menu: '))
+	new_artist = str(raw_input('Enter new artist name or stay blank to exit to menu: '))
 	if (len(new_artist) != 0): cursor.execute(SQL_INSERT_ARTIST, (new_artist,))
-	main_menu()
 
 def add_song():
 	#show and add a songs
 	print('the songs')
-	main_menu()
 	
 def invalid_input():
 	print('Invalid input.  Please try again')
-	main_menu()
 	
 def exit_program():
+	global stay
 	choice = raw_input('Would you like to save changes to the database? (y/n):')
 	if (choice.lower() == 'y'):
 		db_connection.commit()
-		print('Changes saved.\nGoodbye.  Thanks for coming.')
+		print('Changes saved.\nGoodbye.  Thanks for coming!')
+		stay = False
 	elif (choice.lower() == 'n'):
-		print('Changes not saved.\nGoodbye.  Thanks for coming.')
+		print('Changes not saved.\nGoodbye.  Thanks for coming!')
+		stay = False
 	else:
-		print('Invalid input.  Please try again')
+		invalid_input()
 		exit_program()
-	
-	
+
 #connects to the db
 db_connection = sqlite3.connect(DB_FILENAME)
  
 #creates a cursor, a pointer to a point in a collection of data
 cursor = db_connection.cursor() 
 
-main_menu()
+while stay:
+	main_menu()
 
 #closes connection to the db
 db_connection.close()
