e53b3ae14296bd161d4336f6e15bd4974aecf07b
jrscott-goggles <justin@scottfamily.com>
2016-03-23 16:07:15 -0700

    implemented album addition
    add_album() now allows users to view and add an album
    -NOTE: Does not check to ensure artist number is valid.  To be
    implemented.

--- base.py
+++ base.py
@@ -2,17 +2,20 @@ import sqlite3
  
 DB_FILENAME = 'songs.sqlite3.db'
 SQL_SELECT_GENRES = 'SELECT name FROM genres'
-SQL_INSERT_GENRE = "INSERT INTO genres(name) VALUES(?);"
+SQL_INSERT_GENRE = 'INSERT INTO genres(name) VALUES(?);'
+SQL_SELECT_ALBUMS = 'SELECT albums.name, artists.name FROM albums,artists WHERE artists.id  = albums.artist_id'
+SQL_INSERT_ALBUM = 'INSERT INTO albums(name, artist_id) VALUES(?,?);'
+SQL_SELECT_ARTISTS = 'SELECT id, name FROM artists'
 
 def main_menu():
-	print("Welcome to the music database!")
-	print("\t1. Display all song information.")
-	print("\t2. Add a new genere.")
-	print("\t3. Add a new album.")
-	print("\t4. Add a new artist.")
-	print("\t5. Add a new song.")
-	print("\t6. Exit.")
-	choice = raw_input("Enter a choice: ")
+	print('Welcome to the music database!')
+	print('\t1. Display all song information.')
+	print('\t2. Add a new genere.')
+	print('\t3. Add a new album.')
+	print('\t4. Add a new artist.')
+	print('\t5. Add a new song.')
+	print('\t6. Exit.')
+	choice = raw_input('Enter a choice: ')
 	try:
 		choice = int(choice)
 		if (choice == 1): display_songs()
@@ -28,48 +31,57 @@ def main_menu():
 
 def display_songs():
 	#display and add songs
-	print("the songs")
+	print('the songs')
 	main_menu()
 
 def add_genre():
 	#prints genres
-	print("Genres in the database:")
+	print('Genres in the database:')
 	for row in cursor.execute(SQL_SELECT_GENRES):
-		print("%s" % row)
-	new_genre = str(raw_input("Enter new genre name or leave blank to exit to menu:"))
+		print('%s' % row)
+	new_genre = str(raw_input('Enter new genre name or leave blank to exit to menu:'))
 	#if something was entered, add it as a genre
 	if (len(new_genre) != 0): cursor.execute(SQL_INSERT_GENRE,(new_genre,))
 	main_menu()
 	
 
 def add_album():
-	#show and add an albums
-	print("the albums")
+	#prints albums
+	print('Albums in the database:')
+	for row in cursor.execute(SQL_SELECT_ALBUMS):
+		print('Album: %s. Artist: %s' % (row[0], row[1]))
+	new_album = str(raw_input('Enter new album name or leave blank to exit to menu: '))
+	if (len(new_album) != 0):
+		for row in cursor.execute(SQL_SELECT_ARTISTS):
+			print('%d. %s' %(int(row[0]), row[1]))
+		artist = str(raw_input('Enter the number of the artist for this album or leave blank to exit to menu: '))
+		#add error checking for invalid artist
+		if(len(artist) != 0): cursor.execute(SQL_INSERT_ALBUM,(new_album, artist,));
 	main_menu()
 	
 def add_artist():
 	#show and add an artists
-	print("the artists")
+	print('the artists')
 	main_menu()
 
 def add_song():
 	#show and add a songs
-	print("the songs")
+	print('the songs')
 	main_menu()
 	
 def invalid_input():
-	print("Invalid input.  Please try again")
+	print('Invalid input.  Please try again')
 	main_menu()
 	
 def exit_program():
-	choice = raw_input("Would you like to save changes to the database? (y/n):")
+	choice = raw_input('Would you like to save changes to the database? (y/n):')
 	if (choice.lower() == 'y'):
 		db_connection.commit()
-		print("Changes saved.\nGoodbye.  Thanks for coming.")
+		print('Changes saved.\nGoodbye.  Thanks for coming.')
 	elif (choice.lower() == 'n'):
-		print("Changes not saved.\nGoodbye.  Thanks for coming.")
+		print('Changes not saved.\nGoodbye.  Thanks for coming.')
 	else:
-		print("Invalid input.  Please try again")
+		print('Invalid input.  Please try again')
 		exit_program()
 	
 	
